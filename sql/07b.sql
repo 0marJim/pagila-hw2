2a4b6ecb53bb0a58ec0f1aa09edbe129478b263d
0marJim <omar.jimenez.us@gmail.com>
2026-03-11 14:21:53 -0700

    edited sql queries

--- sql/07b.sql
+++ sql/07b.sql
@@ -2,4 +2,18 @@
  * This problem is the same as 07.sql,
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
-
+SELECT DISTINCT title
+FROM film
+JOIN inventory USING (film_id)
+LEFT JOIN (
+    SELECT film_id
+    FROM inventory
+    JOIN rental USING (inventory_id)
+    JOIN customer USING (customer_id)
+    JOIN address USING (address_id)
+    JOIN city USING (city_id)
+    JOIN country USING (country_id)
+    WHERE country.country = 'United States'
+) AS us_rentals USING (film_id)
+WHERE us_rentals.film_id IS NULL
+ORDER BY title;
--- sql/12.sql
+++ sql/12.sql
@@ -6,3 +6,19 @@
  * Create a select statement that lists the titles of all tables with the 'Trailers' special_feature.
  * Inner join the queries above.
  */
+SELECT title
+FROM (
+    SELECT title
+    FROM film
+    WHERE 'Behind the Scenes' IN (
+        SELECT unnest(special_features)
+    )
+) AS behind_the_scenes
+JOIN (
+    SELECT title
+    FROM film
+    WHERE 'Trailers' IN (
+        SELECT unnest(special_features)
+    )
+) AS trailers USING (title)
+ORDER BY title;
